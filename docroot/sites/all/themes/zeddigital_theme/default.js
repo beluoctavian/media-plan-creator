83cec76e759ce52c55ec066c516312a2de6ddfe8
Belu Octavian <belu.octavian@yahoo.com>
2016-03-21 15:08:50 +0200

    Removed old javascript

--- docroot/sites/all/themes/zeddigital_theme/default.js
+++ docroot/sites/all/themes/zeddigital_theme/default.js
@@ -1,16 +1,4 @@
 jQuery(document).ready(function ($) {
-    var bought_adviews = $('input', '.field-name-field-bought-adviews');
-    var net_cpm = $('input', '.field-name-field-cpm');
-    var total_net_budget = $('input', '.field-name-field-total-net-budget');
-    total_net_budget.attr('disabled','disabled');
-    bought_adviews.change(set_total_net_budget);
-    net_cpm.change(set_total_net_budget);
-    function set_total_net_budget() {
-        /**
-         * Compute "Total Net Budget" value.
-         * The formula is: ("Bought adviews" * "Net Cost-Per-Thou (CPM)") / 1000
-         */
-        total_net_budget.val((bought_adviews.val() * net_cpm.val()) / 1000);
-    }
+
 }(jQuery));
 
